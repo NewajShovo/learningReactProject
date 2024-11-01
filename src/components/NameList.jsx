ebae0e0f11e2413a5f6459fd7c0e233ad0e59859
NewajShovo <leoshovo@gmail.com>
2024-11-01 21:32:57 +0600

    Iterating list with key

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -4,15 +4,14 @@ class NameList extends Component {
   render() {
     const names = ["Bruce", "Clark", "Diana"];
     const nameList = names.map((name) => <h2>{name}</h2>);
-    // return (
-    //   <div>
-    //      {names.map((name) => (
-    //       <h2>{name}</h2>
-    //     ))}
-    //   </div>
-
-    // );
-    return <div>{nameList}</div>;
+    return (
+      <div>
+        {names.map((name) => (
+          <h2 key={name}>{name}</h2>
+        ))}
+      </div>
+    );
+    // return <div>{nameList}</div>;
   }
 }
 
