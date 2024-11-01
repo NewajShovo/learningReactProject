e7f5bc7de12efa2fe3fdf7aeb3344712d882b7b6
NewajShovo <leoshovo@gmail.com>
2024-11-01 11:10:15 +0600

    Modifying states

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Welcome from "./components/Welcome";
 
 import Hello from "./components/Hello";
 import Message from "./components/Message";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
+      <Counter />
       {/* <Greet name="Everyone">
         <button>Action</button>
       </Greet>
       <Greet name="Shovo" />
       <Welcome name=" Hello Shovo"></Welcome> */}
       {/* <Hello /> */}
-      <Message />
+      {/* <Message /> */}
     </div>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    // message: "Welcome Visitor",
+    count: 0,
+  };
+
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       count: 0,
+  //     };
+  //   }
+  incrementValue() {
+    this.setState(
+      {
+        count: this.state.count + 1,
+      },
+      () => {
+        console.log("Callback Value", this.state.count);
+      }
+    );
+  }
+
+  handlerIncrementFive() {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  }
+
+  incrementFive() {
+    this.handlerIncrementFive();
+    this.handlerIncrementFive();
+    this.handlerIncrementFive();
+    this.handlerIncrementFive();
+    this.handlerIncrementFive();
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.count}</div>
+        <button onClick={() => this.incrementFive()}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
