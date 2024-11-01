52710f6c8d64f7d5472c1a030af2c2b65582c71a
NewajShovo <leoshovo@gmail.com>
2024-11-01 09:20:38 +0600

    Class component

---
+++ src/components/Welcome.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Welcome extends Component {
+  state = {};
+  render() {
+    return <h1> Class component </h1>;
+  }
+}
+
+export default Welcome;
